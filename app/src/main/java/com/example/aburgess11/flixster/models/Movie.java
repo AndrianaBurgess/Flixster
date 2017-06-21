d060fccbde3db5467bcec7e5dc41e981312b176e
AndrianaBurgess <aburgess11@fb.com>
2017-06-21 16:11:47 -0700

    Initial commit & Network calls working

---
+++ app/src/main/java/com/example/aburgess11/flixster/models/Movie.java
@@ -0,0 +1,34 @@
+package com.example.aburgess11.flixster.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by aburgess11 on 6/21/17.
+ */
+
+public class Movie {
+
+    private String title;
+    private String overview;
+    private String posterPath;
+
+    public Movie(JSONObject object) throws JSONException{
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+}
